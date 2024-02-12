dc21daf295b168a420ed13fbf83aa97fc8b72645
Jamez <adobeclip2003@gmail.com>
2024-02-12 08:13:46 +0100

    Router not needed

--- routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-router.get('/index.html', () => {
-    res.sendFile(path.join(__dirname , 'public', 'index.html'))
-})
-
-module.exports = router;
--- server.js
+++ server.js
@@ -7,7 +7,10 @@ const app = express()
 // server default configs
 app.use(express.static(path.join(__dirname , 'public')))
 app.use(require('./form.js'))
-app.use(router)
+
+app.get('/index.html', () => {
+    res.sendFile(path.join(__dirname , 'public', 'index.html'))
+})
 
 // starting server
 app.listen(process.env.PORT || 8080, () => {
